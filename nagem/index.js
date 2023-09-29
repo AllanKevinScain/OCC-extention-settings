8f05ef0f4218dad5f56e420713f69293d82ad6a9
allankevinscain <meuemail44allan@gmail.com>
2023-09-29 09:43:27 -0300

    chore: gamerzone setting

--- nagem/index.js
+++ nagem/index.js
@@ -1,8 +1,8 @@
-const regex = new RegExp("\\.(png|jpe?g|gif|bmp|webp)$");
-const url = "imagem.bmp";
+const regex = new RegExp("^(0[1-9]|1[0-2])\\/(0[1-9]|[12][0-9]|3[01])\\/\\d{4} (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$");
+const url = "04/04/2023 00:00";
 
 if (regex.test(url)) {
-  console.log("URL válida");
+  console.log("válida");
 } else {
-  console.log("URL inválida");
+  console.log("inválida");
 }
